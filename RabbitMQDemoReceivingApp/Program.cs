03c41635adca2a42b9d1fbf08166733acfb2da1e
albert.calamatta <albert.calamatta@gmail.com>
2023-12-22 23:57:05 -0500

    Add project files.

---
+++ RabbitMQDemoReceivingApp/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Welcome to the ticketing service");
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    UserName = "user",
+    Password = "mypass",
+    VirtualHost = "/"
+};
+
+var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare("bookings", durable: true, exclusive: false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, eventArgs) =>
+{
+    //getting  the byte array
+    var body = eventArgs.Body.ToArray();
+
+    var message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine("A message has been received: " + message);
+};
+
+channel.BasicConsume("bookings", true, consumer);
+
+Console.ReadKey();
+
